55c9984c5f8b184085fb30f1e7b6137e9ac4f6af
dnyanesh181999 <dnyaneshdeshmukh506@gmail.com>
2024-06-06 15:48:36 +0530

    Second commit By pranav Deshmukh

--- microservice_oe/src/main/java/com/cjc/loanapplication/controller/OeController.java
+++ microservice_oe/src/main/java/com/cjc/loanapplication/controller/OeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.cjc.loanapplication.servicei.OeServicei;
 import jakarta.persistence.Entity;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
+@CrossOrigin("*")
 @RestController
 @RequestMapping("oe")
 public class OeController {
